551b9aed6905c2d099d22fb050bf8157e974436d
JackElvinsson <jackelvinsson@gmail.com>
2023-02-04 14:40:36 +0100

    added class reportTasks

---
+++ src/DatabaseUtils/ReportTasks.java
@@ -0,0 +1,16 @@
+package DatabaseUtils;
+
+import Customer.Customer;
+import Order.Order;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ReportTasks {
+
+    List<Order> orders;
+    List<Customer> customers;
+
+
+}
